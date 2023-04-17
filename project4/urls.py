0d6ffb08267b4e2b28fd0552a2d3fe66dfb69822
Aliona83 <alionajanusko@gmail.com>
2023-04-17 09:51:59 +0000

    Modify login templates

--- project4/urls.py
+++ project4/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipe.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
 
 
