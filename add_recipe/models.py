e99be50a67a1536a7bb914316cd4a1e8dba8e368
Aliona83 <alionajanusko@gmail.com>
2023-04-18 12:53:31 +0000

    Install pillow to be able add and resize images

--- add_recipe/models.py
+++ add_recipe/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from djrichtextfield.models import RichTextField
+from django_resized import ResizedImageField
+
+MEAL_TYPES = (
+    ('breakfast', 'Breakfast'),
+    ('lunch', 'Lunch'),
+    ('dinner', 'Dinner')
+)
 
 
 class recipes(models.Model):
@@ -9,4 +17,14 @@ class recipes(models.Model):
     user = models.ForeignKey(User, related_name='recipe_owner', on_delete=models.CASCADE)
     title = models.CharField(max_length=300, null=False, blank=False)
     description = models.CharField(max_length=500, null=False, blank=False)
-    
+    ingredients = RichTextField(max_length=1000, null=False, blank=False)
+    image = ResizedImageField(size=[400, None], quality=75, upload_to='add_recipe/', force_format='WEBP', blank=False, nul=False)
+    image_alt = models.CharField(max_length=100, null=False, blank=False)
+    meal_type = models.CharField(max_length=50, choices=MEAL_TYPES, default='breakfast')
+    posted_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-posted_date']
+
+    def __str__(self):
+        return str(self.title)
