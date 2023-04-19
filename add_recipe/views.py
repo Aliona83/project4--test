191cd6f78b43d6f5d367cbb7ed6a1950eefe709c
Aliona83 <alionajanusko@gmail.com>
2023-04-19 12:52:19 +0000

    Create new view for AddRecipe,import recipes from modules

--- add_recipe/views.py
+++ add_recipe/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from .modules import recipes
+
 
 # Create your views here.
+class AddRecipe(CreateView):
+    template_name = 'add_recipe/add_recipe.html'
+    model = recipes
+    success_url = '/add_recipe/'
+
+    def form_valid(sel, form):
+        form.instance.user = self.request.user
+        return super(AddRecipe, self).form_valid(form)
