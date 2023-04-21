ab7ee1cb59a2ca036f074f78c4f6771df14747f2
Aliona83 <alionajanusko@gmail.com>
2023-04-21 12:27:10 +0000

    Change style for cards

--- add_recipe/admin.py
+++ add_recipe/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import recipes
 # Register your models here.
 
+
 @admin.register(recipes)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = (
--- add_recipe/urls.py
+++ add_recipe/urls.py
@@ -3,6 +3,6 @@ from .views import AddRecipe, All_Recipes
 
 
 urlpatterns = [
-    path("", AddRecipe.as_view(), name='add_recipe'),
-    path("all_recipes/", All_Recipes.as_view(), name="all_recipes"),
+    path("add/", AddRecipe.as_view(), name='add_recipe'),
+    path("", All_Recipes.as_view(), name="all_recipes"),
 ]
