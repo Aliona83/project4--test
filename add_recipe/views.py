e782cb203f2e564a5177156acb5ec5a3cd86cb72
Aliona83 <alionajanusko@gmail.com>
2023-04-21 10:07:45 +0000

    Add block content to all_recipes.html file

--- add_recipe/views.py
+++ add_recipe/views.py
@@ -2,6 +2,7 @@ from django.views.generic import CreateView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import recipes
 from .forms import RecipeForm
+from django.db.models import Q
 
 
 class All_Recipes(ListView):
@@ -11,6 +12,19 @@ class All_Recipes(ListView):
     template_name = "add_recipe/all_recipes.html"
     model = recipes
     context_object_name = "all_recipes"
+
+    def get_queryset(self, **kwargs):
+        query = self.request.GET.get('q')
+        if query:
+            recipes = self.model.objects.filter(
+                Q(title__icontains=query) |
+                Q(description__icontains=query) |
+                Q(instructions__icontains=query) |
+                Q(cuisine_types__icontains=query)
+            )
+        else:
+            recipes = self.model.objects.all()
+        return recipes
 
 
 class AddRecipe(LoginRequiredMixin, CreateView):
@@ -22,6 +36,6 @@ class AddRecipe(LoginRequiredMixin, CreateView):
     form_class = RecipeForm
     success_url = '/add_recipe/'
 
-    def form_valid(sel, form):
+    def form_valid(self, form):
         form.instance.user = self.request.user
         return super(AddRecipe, self).form_valid(form)
